4b1a127590a169b1597b7bbd0c1cccf081ac8e4c
amansj1 <51205338+amansj1@users.noreply.github.com>
2022-06-05 22:52:21 +0700

    done

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
-
+const sorter = (names,nub) => {
+  const idx = nub(names);
+  const nameidx = [];
+  for (i = 0; i < idx.length; i++) {
+    nameidx[i] = `${i + 1}. + ${idx[i]}`;
+  }
+  return nameidx;
+}
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (nameidx) => {
+  return nameidx.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (nameidx) => {
+  return nameidx.sort().reverse();
+}
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
